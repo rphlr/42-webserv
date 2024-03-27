011bfdfe4de3887038ba5b9c142c820e3484c948
natevaub <nathan.vaubien@gmail.com>
2024-03-27 16:45:21 +0100

    Update

--- srcs/Server/HandleRequest.cpp
+++ srcs/Server/HandleRequest.cpp
@@ -27,13 +27,34 @@ void HandleRequest::handleRequest() {
 	std::cout << "Path: " << _path << std::endl;
 	std::cout << "Protocol: " << _protocol << std::endl;
 
-
 	// Next lines are the headers
-	
-	
-	// Last line is the body
-	std::cout << "\n\n\n";
+	size_t headers_start = _request.find("\n") + 1;
+	size_t headers_end = _request.find("\n\n");
+	std::string headers_str = _request.substr(headers_start, headers_end - headers_start);
+	std::stringstream headers_stream(headers_str);
+	std::string header_line;
+	while (std::getline(headers_stream, header_line)) {
+		size_t separator = header_line.find(": ");
+		if (separator != std::string::npos) {
+			std::string header_name = header_line.substr(0, separator);
+			std::string header_value = header_line.substr(separator + 2);
+			_headers[header_name] = header_value;
+		}
+	}
 
+	// Last line is the body
+	if (headers_end != std::string::npos) {
+		_body = _request.substr(headers_end + 2);
+	}
+
+	std::cout << "!!! Headers parsed !!!\n";
+	// Loop over the header the c+98 way
+	for (std::map<std::string, std::string>::iterator it = _headers.begin(); it != _headers.end(); ++it) {
+		std::cout << it->first << ": " << it->second << std::endl;
+	}
+
+	std::cout << "!!! Body !!!\n";
+	std::cout << _body << std::endl;
 }
 
 HandleRequest::~HandleRequest() {}
